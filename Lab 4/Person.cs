05d362d4c197d303decfadd59df042f3d88f4e92
KIAN WILKINSON <K.MICHAEL-WILKINSON-2020@hull.ac.uk>
2021-02-27 14:09:43 +0000

    created a person class and found the age

---
+++ Lab 4/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_4
+{
+    class Person
+    {
+        public string Name { get; private set;  }
+        public DateTime DateOfBirth { get; private set; }
+        public int Age { get { return CalculateAge(); } }
+
+        public Person(string pName, DateTime pDateOfBirth)
+        {
+            Name = pName;
+            DateOfBirth = pDateOfBirth;
+        }
+
+        private int CalculateAge()
+        {
+            DateTime whenIsNow = DateTime.Now;
+            TimeSpan difference = whenIsNow - DateOfBirth;
+            int daysInYear = 365;
+            int numberOfDays = difference.Days;
+            int age = numberOfDays / daysInYear;
+            return age;
+        }
+    }
+}
